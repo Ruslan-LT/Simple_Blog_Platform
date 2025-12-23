902bdd3184407f653e670d4625b46cf7c30ea4ed
PysJlaH_JlT <ruslan160306@gmail.com>
2025-12-23 18:50:13 +0200

    Виправлено requirements.txt

--- SimpleBlogPlatformProject/settings.py
+++ SimpleBlogPlatformProject/settings.py
@@ -142,6 +142,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
